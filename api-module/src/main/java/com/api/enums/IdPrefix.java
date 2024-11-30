3b74f130e94b1a1b99b446dd3c2f8636443dd69a
zilzu <zilzu94@gmail.com>
2024-11-30 23:19:01 +0900

    [feat] 1. IdGenerator 구현 : Id를 부여하는 Generator id 생성 rule : {prefix}_UUID

---
+++ api-module/src/main/java/com/api/enums/IdPrefix.java
@@ -0,0 +1,12 @@
+package com.api.enums;
+
+public enum IdPrefix {
+
+    MDL,    /*model*/
+    PHO,    /*photographer*/
+    MSG,    /*message*/
+    CON,    /*conversation*/
+    MAT,    /*matching*/
+    FIL     /*file*/
+
+}
---
+++ api-module/src/main/java/com/api/util/IdGenerator.java
@@ -0,0 +1,18 @@
+package com.api.util;
+
+import com.api.enums.IdPrefix;
+import org.springframework.stereotype.Component;
+
+/**
+ * Id를 부여하는 Generator
+ * id 채번 rule : {prefix}_UUID
+ * prefix : IdPrefix
+ *
+ */
+@Component
+public class IdGenerator {
+
+    public String generateId(IdPrefix prefix) {
+        return prefix + "_" + java.util.UUID.randomUUID().toString();
+    }
+}
