8e76f1729b9bb3cfc595dd3d16a7327404480aaf
zilzu <zilzu94@gmail.com>
2024-12-01 18:11:12 +0900

    [feat] 1. conversation, matching, message 서비스 구현

--- api-module/src/main/java/com/api/service/MessageService.java
+++ api-module/src/main/java/com/api/service/MessageService.java
@@ -1,11 +1,14 @@
 package com.api.service;
 
+import com.api.dto.message.ConversationMessageStatusResponse;
+import com.api.enums.IdPrefix;
 import com.api.repository.ConversationRepository;
 import com.api.dto.message.MessageRequest;
 import com.api.dto.message.MessageResponse;
 import com.api.entity.MessageEntity;
 import com.api.enums.MessageStatus;
 import com.api.repository.MessageRepository;
+import com.api.util.IdGenerator;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -15,29 +18,31 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
     private final ConversationRepository conversationRepository;
+    private final IdGenerator idGenerator;
 
-    public MessageService(MessageRepository messageRepository, ConversationRepository conversationRepository) {
+    public MessageService(MessageRepository messageRepository, ConversationRepository conversationRepository, IdGenerator idGenerator) {
         this.messageRepository = messageRepository;
         this.conversationRepository = conversationRepository;
+        this.idGenerator = idGenerator;
     }
 
     public MessageResponse send(MessageRequest messageRequest) {
 
-        /**
-         * todo
-         * 1. 대화방이 아직 생성되지 않았다면 대화방 생성 후 메시지 생성!
-         */
-
-        String conversationId = conversationRepository.getConversationId(messageRequest);
-
-        if (conversationId == null) {
-            conversationId = conversationRepository.createConversation(messageRequest);
+        if (messageRequest.getFileId() != null && messageRequest.getMessage() != null) {
+            throw new IllegalArgumentException("fildId와 message 필드가 동시에 존재할 수 없습니다.");
+        }else if(messageRequest.getFileId() == null && messageRequest.getMessage() == null){
+            throw new IllegalArgumentException("fileId와 message 필드중 적어도 하나는 있어야 합니다.");
         }
 
-        String messageId = "msg_001";  // todo
+        String conversationId = conversationRepository.getConversationId(messageRequest);
+        String generatedMessageId = idGenerator.generateId(IdPrefix.MSG);
+
+        if (conversationId == null) {
+            conversationId = conversationRepository.createConversation(generatedMessageId, messageRequest);
+        }
 
         MessageEntity messageEntity = messageRepository.createMessage(new MessageEntity(
-                messageId,
+                generatedMessageId,
                 conversationId,
                 messageRequest.getSenderId(),
                 messageRequest.getReceiverId(),
@@ -49,7 +54,10 @@ public class MessageService {
         return new MessageResponse(messageEntity.getMessageId(), messageEntity.getConversationId(), messageEntity.getSenderId(), messageEntity.getReceiverId(), messageEntity.getTimestamp(), messageEntity.getMessageStatus());
     }
 
-    public void updateStatusRead(String conversationId, String readerId) {
-        messageRepository.updateStatusRead(conversationId, readerId);
+    public ConversationMessageStatusResponse updateStatusRead(String conversationId, String readerId) {
+
+        MessageEntity messageEntity = messageRepository.updateStatusRead(conversationId, readerId);
+
+        return new ConversationMessageStatusResponse(messageEntity.getConversationId(), messageEntity.getMessageStatus());
     }
 }
