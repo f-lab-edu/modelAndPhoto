4abd277db0fc7f9cd6a43c412430fa22d565285c
zilzu <zilzu94@gmail.com>
2024-12-09 00:37:04 +0900

    [feat] 1. fileController 서비스, 테스트 구현

--- api-module/src/main/java/com/api/controller/FileController.java
+++ api-module/src/main/java/com/api/controller/FileController.java
@@ -1,13 +1,14 @@
 package com.api.controller;
 
-import com.api.dto.file.FileUploadResponse;
+import com.api.dto.file.PresingedUrlResult;
 import com.api.service.FileService;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/files")
@@ -19,27 +20,37 @@ public class FileController {
         this.fileService = fileService;
     }
 
+    // presignedPutUrl
+    @GetMapping("/generate-upload-url")
+    public ResponseEntity<PresingedUrlResult> generatePresignedPutUrl(@RequestParam String fileId,
+                                                                      @RequestParam String contentType) {
+
+        PresingedUrlResult presingedUrlResult = fileService.generatePresignedPutUrl(fileId, contentType);
+
+        return ResponseEntity.ok(presingedUrlResult);
+    }
+
     // 파일 업로드
     @PostMapping("/upload")
-    public ResponseEntity<FileUploadResponse> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> uploadFile(@RequestParam MultipartFile file,
+                                             @RequestParam String presignedUrl) throws IOException {
 
-        FileUploadResponse fileUploadResponse = fileService.upload(file);
+        fileService.uploadFile(presignedUrl, file);
 
-        return ResponseEntity.ok(fileUploadResponse);
+        return ResponseEntity.ok("ok");
     }
 
     // 파일 다운로드
     @GetMapping("/{fileId}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId,
+                                                 @RequestParam String contentType) throws IOException {
 
-        // 실제 파일 데이터를 ByteArrayResource로
-        byte[] fileData = "this is a test file".getBytes();
-        ByteArrayResource resource = new ByteArrayResource(fileData, "application/octet-stream");
+        // todo
 
-        fileService.download(fileId);
+//        fileService.download(fileId);
 
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+                .body(null);
     }
 }
