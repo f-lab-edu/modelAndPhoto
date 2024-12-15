68cc07aeb1ab683331e181392563a92f5b0e193b
Seongsu Park <72535219+zilzu4165@users.noreply.github.com>
2024-12-15 17:31:44 +0900

    [test] AwsS3PropertiesTest

--- api-module/src/test/java/com/api/config/AwsS3PropertiesTest.java
+++ api-module/src/test/java/com/api/config/AwsS3PropertiesTest.java
@@ -2,36 +2,34 @@ package com.api.config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestConfiguration
-@EnableConfigurationProperties(AwsS3Properties.class)
+@SpringBootTest
 class AwsS3PropertiesTest {
 
-    @Bean
-    public AwsS3Properties awsS3Properties() {
-        return new AwsS3Properties();
-    }
+    @Autowired
+    public AwsS3Properties awsS3Properties;
 
     @Test
-    @DisplayName("AwsS3Properties 에 설정값이 잘 바인딩 되는지 확인한다.")
+    @DisplayName("AwsS3Properties 에 설정값이 잘 바인딩 되었는지 확인한다.")
     void testAwsS3PropertiesBinding() {
-        AwsS3Properties awsS3Properties = new AwsS3Properties();
 
-        awsS3Properties.getCredentials().setAccessKey("test-access-key");
-        awsS3Properties.getCredentials().setSecretKey("test-secret-key");
-        awsS3Properties.getRegion().setRegionStatic("us-east-1");
-        awsS3Properties.getS3().setBucket("test-bucket");
-        awsS3Properties.getS3().setEndpoint("https://test-endpoint.com");
-
-        assertThat(awsS3Properties.getCredentials().getAccessKey()).isEqualTo("test-access-key");
-        assertThat(awsS3Properties.getCredentials().getSecretKey()).isEqualTo("test-secret-key");
-        assertThat(awsS3Properties.getRegion().getRegionStatic()).isEqualTo("us-east-1");
-        assertThat(awsS3Properties.getS3().getBucket()).isEqualTo("test-bucket");
-        assertThat(awsS3Properties.getS3().getEndpoint()).isEqualTo("https://test-endpoint.com");
+        assertThat(awsS3Properties.getCredentials().getAccessKey()).isNotNull();
+        assertThat(awsS3Properties.getCredentials().getSecretKey()).isNotNull();
+        assertThat(awsS3Properties.getRegion().getRegionStatic()).isNotNull();
+        assertThat(awsS3Properties.getS3().getBucket()).isNotNull();
+        assertThat(awsS3Properties.getS3().getEndpoint()).isNotNull();
     }
 }
