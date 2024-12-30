2c974625d26cf5ae6cefe88bed349e598e9b2cc2
Seongsu Park <72535219+zilzu4165@users.noreply.github.com>
2024-12-30 14:23:19 +0900

    [feat] 대화방 생성로직 분리

--- api-module/src/main/java/com/api/service/MessageService.java
+++ api-module/src/main/java/com/api/service/MessageService.java
@@ -6,7 +6,6 @@ import com.api.dto.message.MessageResponse;
 import com.api.entity.ConversationEntity;
 import com.api.entity.MessageEntity;
 import com.api.enums.MessageStatus;
-import com.api.repository.ConversationRepository;
 import com.api.repository.MessageRepository;
 import com.api.util.IdGenerator;
 import org.springframework.stereotype.Service;
@@ -20,26 +19,28 @@ import java.util.List;
 public class MessageService {
 
     private final MessageRepository messageRepository;
-    private final ConversationRepository conversationRepository;
+    private final ConversationService conversationService;
 
-    public MessageService(MessageRepository messageRepository, ConversationRepository conversationRepository) {
+    public MessageService(MessageRepository messageRepository, ConversationService conversationService) {
         this.messageRepository = messageRepository;
-        this.conversationRepository = conversationRepository;
+        this.conversationService = conversationService;
     }
 
+    @Transactional
     public MessageResponse send(MessageRequest messageRequest) {
 
         if (messageRequest.getFileId() != null && messageRequest.getMessage() != null) {
             throw new IllegalArgumentException("fildId와 message 필드가 동시에 존재할 수 없습니다.");
-        }else if(messageRequest.getFileId() == null && messageRequest.getMessage() == null){
+        } else if (messageRequest.getFileId() == null && messageRequest.getMessage() == null) {
             throw new IllegalArgumentException("fileId와 message 필드중 적어도 하나는 있어야 합니다.");
         }
 
-        String conversationId = conversationRepository.getConversationId(messageRequest.getSenderId(), messageRequest.getReceiverId()).orElse(null);
         String generatedMessageId = IdGenerator.getGenerateMessageId();
 
-        if (conversationId == null) {
-            ConversationEntity saved = conversationRepository.save(new ConversationEntity(IdGenerator.getGenerateConversationId(), List.of(messageRequest.getSenderId(), messageRequest.getReceiverId()), LocalDateTime.now(), LocalDateTime.now(), LocalDateTime.now()));
+        String conversationId = messageRequest.getConversationId();
+
+        if (messageRequest.getConversationId() == null) {
+            ConversationEntity saved = conversationService.createConversation(IdGenerator.getGenerateConversationId(), List.of(messageRequest.getSenderId(), messageRequest.getReceiverId()));
             conversationId = saved.getConversationId();
         }
 
--- api-module/src/test/java/com/api/service/MessageServiceTest.java
+++ api-module/src/test/java/com/api/service/MessageServiceTest.java
@@ -5,7 +5,6 @@ import com.api.dto.message.MessageRequest;
 import com.api.dto.message.MessageResponse;
 import com.api.entity.MessageEntity;
 import com.api.enums.MessageStatus;
-import com.api.repository.ConversationRepository;
 import com.api.repository.MessageRepository;
 import com.api.util.IdGenerator;
 import org.junit.jupiter.api.DisplayName;
@@ -14,12 +13,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -32,20 +31,15 @@ class MessageServiceTest {
     @Mock
     private MessageRepository messageRepository;
 
-    @Mock
-    private ConversationRepository conversationRepository;
-
     @Test
     @DisplayName("메시지 전송 시 response에 conversationId가 생성된다.")
     void test_send() {
         // given
         String generatedConversationID = IdGenerator.getGenerateConversationId();
-        MessageRequest messageRequest = new MessageRequest("MOD_123", "PHO_456", "메시지내용", null);
+        MessageRequest messageRequest = new MessageRequest("MOD_123", "PHO_456", generatedConversationID, "메시지내용", null);
 
         doReturn(new MessageEntity("MSG_001", generatedConversationID, "MOD_123", "PHO_456", null, "메시지내용", LocalDateTime.now(), MessageStatus.SENT))
                 .when(messageRepository).save(any(MessageEntity.class));
-
-        doReturn(Optional.of(generatedConversationID)).when(conversationRepository).getConversationId(any(String.class), any(String.class));
 
         // when
         MessageResponse messageResponse = messageService.send(messageRequest);
@@ -55,7 +49,6 @@ class MessageServiceTest {
         assertThat(messageResponse.getConversationId()).isEqualTo(generatedConversationID);
 
         // verify
-        verify(conversationRepository, times(1)).getConversationId(any(String.class), any(String.class));
         verify(messageRepository, times(1)).save(any(MessageEntity.class));
     }
 
@@ -63,7 +56,7 @@ class MessageServiceTest {
     @DisplayName("메시지 messageRequest에 fileId와 message 필드가 동시에 존재하면 IllegalArgumentException을 던진다.")
     void test_message_request_file_id_and_message_content_not_exist_at_the_same_time() {
         // given
-        MessageRequest messageRequest = new MessageRequest("MOD_123", "PHO_456", "메시지내용", "FIL_123");
+        MessageRequest messageRequest = new MessageRequest("MOD_123", "PHO_456", "CON_001", "메시지내용", "FIL_123");
 
         // then
         assertThatIllegalArgumentException().isThrownBy(() -> messageService.send(messageRequest));
